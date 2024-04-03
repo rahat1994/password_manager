d13b98394ec9488d76b4f35b6f15307e8e91fc1b
Rahat Baksh <rahat392@gmail.com>
2024-04-04 01:39:02 +0600

    Add new images and update .gitignore

--- resources/admin/Modules/Tools/Tools.vue
+++ resources/admin/Modules/Tools/Tools.vue
@@ -1,7 +1,42 @@
 <template>
-    <div v-if="!loadingItems" class="content" style="background-color: #f5f7fa;">
+    <div v-if="!loading" class="content" style="background-color: #f5f7fa; padding: 20px 10px;">
         <div>
-            <h1>Tools</h1>
+            <h1>{{ $t("Generator") }}</h1>
+        </div>
+
+        <div>
+            {{ generatePassword }}
+        </div>
+
+        <div>
+            <el-form ref="form" :model="form" label-width="10rem">
+
+                <el-form-item label="Options">
+                    <el-checkbox-group v-model="form.type">
+                        <el-checkbox label="A-Z" name="type"></el-checkbox>
+                        <el-checkbox label="a-z" name="type"></el-checkbox>
+                        <el-checkbox label="0-9" name="type"></el-checkbox>
+                        <el-checkbox label="!@#$%^&*" name="type"></el-checkbox>
+                    </el-checkbox-group>
+                </el-form-item>
+
+                <el-form-item label="Length">
+                    <el-input v-model="form.name"></el-input>
+                </el-form-item>
+
+                <el-form-item label="Minimum Numbers">
+                    <el-input v-model="form.minimumNumbers"></el-input>
+                </el-form-item>
+
+                <el-form-item label="Minimum Special">
+                    <el-input v-model="form.minimumSpecials"></el-input>
+                </el-form-item>
+
+                <el-form-item>
+                    <el-button type="primary" @click="onSubmit">Regenerate Password</el-button>
+                    <el-button>Cancel</el-button>
+                </el-form-item>
+            </el-form>
         </div>
     </div>
 </template>
@@ -12,6 +47,17 @@
         data() {
             return {
                 loading: false,
+                generatePassword: "G3Ed8n%oVn",
+                form: {
+                    name: '',
+                    region: '',
+                    date1: '',
+                    date2: '',
+                    delivery: false,
+                    type: [],
+                    resource: '',
+                    desc: ''
+                }
             }
         },
     }
